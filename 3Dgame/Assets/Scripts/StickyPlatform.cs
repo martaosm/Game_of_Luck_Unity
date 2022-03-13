867e6830f62e41c581ef5875dcde7a9f2091ff8b
Marta <marta.osm00@gmail.com>
2022-03-13 22:32:49 +0100

    Added start and end menu

--- 3Dgame/Assets/Scripts/StickyPlatform.cs
+++ 3Dgame/Assets/Scripts/StickyPlatform.cs
@@ -6,7 +6,7 @@ public class StickyPlatform : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player" )
         {
             collision.gameObject.transform.SetParent(transform);
         }
---
+++ 3Dgame/Assets/StartButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    /*public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }*/
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
